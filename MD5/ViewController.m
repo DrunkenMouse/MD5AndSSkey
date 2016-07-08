22f96d9e052ceca33c957d20b3473002bdc4dcb9
残月 <632342527@qq.com>
2016-07-08 19:56:35 +0800

    MD5AndSSkey
    MD5和钥匙串

--- MD5/ViewController.m
+++ MD5/ViewController.m
@@ -7,7 +7,8 @@
 //
 
 #import "ViewController.h"
-
+#import "NSString+Hash.h"
+#import "SSKeychain.h"
 @interface ViewController ()
 
 @end
@@ -16,7 +17,44 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    NSString *str = @"http:///login.php";
+    NSURL *url = [NSURL URLWithString:str];
+    NSMutableURLRequest *request = [[NSMutableURLRequest alloc]initWithURL:url];
+    
+//    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+//    NSString *username = [defaults objectForKey:@"username"];
+//    NSString *password = [defaults objectForKey:@"password"];
+
+    NSString *username = [SSKeychain passwordForService:[NSBundle mainBundle].bundleIdentifier account:@"username"];
+    NSString *password = [SSKeychain passwordForService:[NSBundle mainBundle].bundleIdentifier account:@"password"];
+    
+    if (username!=nil&&password!=nil) {
+        
+    }else{
+        username = @"username";
+        password = @"password";
+     
+        password = [password md5String];
+        
+        [SSKeychain setPassword:username forService:[NSBundle mainBundle].bundleIdentifier account:@"username"];
+        [SSKeychain setPassword:password forService:[NSBundle mainBundle].bundleIdentifier account:@"password"];
+    }
+    NSLog(@"%@---%@",username,password);
+    
+    request.HTTPMethod = @"POST";
+    NSString *pargarm = [NSString stringWithFormat:@"username=%@&password=%@",username,password];
+    request.HTTPBody = [pargarm dataUsingEncoding:NSUTF8StringEncoding];
+    
+    [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue  mainQueue] completionHandler:^(NSURLResponse * _Nullable response, NSData * _Nullable data, NSError * _Nullable connectionError) {
+        
+        NSDictionary *dict = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
+        NSLog(@"%@",dict);
+        
+        
+        
+    }];
+    
 }
 
 - (void)didReceiveMemoryWarning {
